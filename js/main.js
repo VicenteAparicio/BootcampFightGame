58bcc13ccfdf46a74839f4fdee39976a31f27410
Goodbyez <miradadesigual@gmail.com>
2021-05-10 19:22:43 +0200

    Movement limits to my fighter

--- js/main.js
+++ js/main.js
@@ -5,11 +5,11 @@ let y_pos=0;
 let velX=1;
 let velY=1;
 function movementX(x){
-    let fighter =  document.getElementById('fighter') //obtengo posición del div con id fighter 
-    fighter.style.left=x+'em'; //añado el sumatorio de la variable al style left de la posición del div con id fighter
+    let fighter =  document.getElementById('fighter'); //obtengo posición del div con id fighter 
+    fighter.style.left=x+'em'; //añado el sumatorio de la variable al style left de la posición del div con id 
 }
 function movementY(y){
-    let fighter =  document.getElementById('fighter') 
+    let fighter =  document.getElementById('fighter');    
     fighter.style.top=y+'em';
 }
 
@@ -18,19 +18,27 @@ function pusher (elEvento){
     console.log(evento.keyCode) //verificación para mí del código de la tecla
     switch(evento.keyCode){
         case 39: //DERECHA
-            x_pos+=velX;
+            if (x_pos>=0 && x_pos<20){
+                x_pos+=velX;
+            }
             movementX(x_pos);
             break;
         case 37: //IZQUIERDA
-            x_pos-=velX;
+            if (x_pos>0 && x_pos<=20){
+                x_pos-=velX;
+            }
             movementX(x_pos);
             break;
         case 38: //ARRIBA
-            y_pos-=velY;
+            if (y_pos>0 && y_pos<=10){
+                y_pos-=velY;
+            }
             movementY(y_pos);
             break;
         case 40: //ABAJO
-            y_pos+=velY;
+            if (y_pos>=0 && y_pos<10){
+                y_pos+=velY;
+            }
             movementY(y_pos);
             break;
     }
