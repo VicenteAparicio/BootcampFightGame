69a26d6b0425b94e499e996ef79f0380263a45aa
Goodbyez <miradadesigual@gmail.com>
2021-05-13 16:55:44 +0200

    Movement Fighter

--- js/main.js
+++ js/main.js
@@ -1,37 +1,42 @@
 // MOVEMENT FIGHTER
 let x_pos=0;
-let y_pos=0;
+// let y_pos=0;
 
-let velX=1;
-let velY=1;
-function movementX(x){
-    let fighter = document.getElementById('fighter'); //obtengo posición del div con id fighter 
-    fighter.style.left=x+'em'; //añado el sumatorio de la variable al style left de la posición del div con id 
-}
-function movementY(y){
-    let fighter = document.getElementById('fighter');    
-    fighter.style.top=y+'em';
-}
-let sizeY = 40;
-let sizeX = 40;
+let velX=30;
+// let velY=1;
+function movementX(x, fighters){
+    let fighter1 = document.getElementById(fighters); //obtengo posición del div con id fighter 
+    fighter1.style.left= x +'em'; //añado el sumatorio de la variable al style left de la posición del div con id 
+};
+// function movementY(y){
+//     let fighter = document.getElementById('fighter');    
+//     fighter.style.top=y+'em';
+// }
+
+let sizeX = 90;
+// let sizeY = 40;
+
 function pusher (elEvento){
     let evento = window.event || elEvento; //almacena info de la tecla
-    let stromboly = document.getElementById('scenario');
-    stromboly.style.height = sizeY +'em';
+    let stromboly = document.getElementById('topScene');
     stromboly.style.width = sizeX +'em';
+    // stromboly.style.height = sizeY +'em';
 
     switch(evento.keyCode){
+        // case 13:
+        //     changeFase('chooseF');
+        //     break;
         case 39: //DERECHA
-            if (x_pos<sizeX-1){
+            if (x_pos<59){
                 x_pos+=velX;
             }
-            movementX(x_pos);
+            movementX(x_pos, 'topFighter');
             break;
         case 37: //IZQUIERDA
             if (x_pos>0){
                 x_pos-=velX;
             }
-            movementX(x_pos);
+            movementX(x_pos, 'topFighter');
             break;
         case 38: //ARRIBA
             if (y_pos>0){
@@ -46,21 +51,34 @@ function pusher (elEvento){
             movementY(y_pos);
             break;
     }
-}
+};
 function systemOn(){
     document.onkeydown = pusher; //capta la pulsación de la tecla y llama a la función pusher
-}
+};
 systemOn();
 
 // CHANGE FASE
-const changeFase = (destino) => {
-    let arrFase = ["start","chooseF","fightScreen", 'victoryScreen'];
-    arrFase = arrFase.filter(val => !destino.includes(val));
-    document.getElementById(destino).style.display = "flex";
-    for(let _fase of arrFase){
+// const changeFase = (destino) => {
+//     let arrFase = ["start", "chooseF", "fightScreen", 'victoryScreen'];
+//     arrFase = arrFase.filter(val => !destino.includes(val));
+//     document.getElementById(destino).style.display = "flex";
+//     for(let _fase of arrFase){
+//         document.getElementById(_fase).style.display = "none";
+//     }
+// };
+
+const changeFase = (destiny) => {
+    let arrayFaseId = [];
+    let arrayFase = document.getElementsByTagName('section');
+    for (let i = 0; i <=arrayFase.length-1; i++){
+        arrayFaseId.push(arrayFase[i].id);
+    }
+    arrayFaseFade = arrayFaseId.filter(val => !destiny.includes(val));
+    document.getElementById(destiny).style.display = "flex";
+    for(let _fase of arrayFaseFade){
         document.getElementById(_fase).style.display = "none";
     }
-};
+}
 
 /* DRAG AND DROP FUNCTION */
 let validate = 1;
