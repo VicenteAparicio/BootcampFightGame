9085433899e74bd11d4994d02d4e5f2cf36c4382
Goodbyez <miradadesigual@gmail.com>
2021-05-15 11:47:22 +0200

    Completed basic defense/attack animation and functional  battle fight  with handmade images for 6 fighters

--- js/main.js
+++ js/main.js
@@ -10,99 +10,128 @@ let velX=33;
 
 const movementAndAttack = (elEvento) => {
     let evento = window.event || elEvento; //almacena info de la tecla
+    if (team2[1].life > 0 && team1[1].life > 0){
+        switch(evento.keyCode){
 
-    switch(evento.keyCode){
+            case 39: //DERECHA LEFT FLECHA DERECHA
+                if (xPosR<59){
+                    xPosR+=velX;
+                }
+                console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+                movementX(xPosR, 'rightFighter');
+                console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+                break;
 
-        case 39: //DERECHA LEFT FLECHA DERECHA
-            if (xPosR<59){
-                xPosR+=velX;
-            }
-            console.log(`Posición R ${xPosR} y l  ${xPosL}`)
-            movementX(xPosR, 'rightFighter');
-            console.log(`Posición R ${xPosR} y l  ${xPosL}`)
-            break;
+            case 37: //IZQUIERDA LEFT FLECHA IZQUIERDA
+                if (xPosR>0){
+                    xPosR-=velX;
+                }
+                console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+                movementX(xPosR, 'rightFighter');
+                console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+                break;
 
-        case 37: //IZQUIERDA LEFT FLECHA IZQUIERDA
-            if (xPosR>0){
-                xPosR-=velX;
-            }
-            console.log(`Posición R ${xPosR} y l  ${xPosL}`)
-            movementX(xPosR, 'rightFighter');
-            console.log(`Posición R ${xPosR} y l  ${xPosL}`)
-            break;
+            case 66: // DERECHA RIGHT
+                if (xPosL<59){
+                    xPosL+=velX;
+                }
+                console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+                movementX(xPosL, 'leftFighter');
+                console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+                break;
 
-        case 66: // DERECHA RIGHT
-            if (xPosL<59){
-                xPosL+=velX;
-            }
-            console.log(`Posición R ${xPosR} y l  ${xPosL}`)
-            movementX(xPosL, 'leftFighter');
-            console.log(`Posición R ${xPosR} y l  ${xPosL}`)
-            break;
+            case 86: // IZQUIERDA RIGHT
+                if (xPosL>0){
+                    xPosL-=velX;
+                }
+                console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+                movementX(xPosL, 'leftFighter');
+                console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+                break;
 
-        case 86: // IZQUIERDA RIGHT
-            if (xPosL>0){
-                xPosL-=velX;
-            }
-            console.log(`Posición R ${xPosR} y l  ${xPosL}`)
-            movementX(xPosL, 'leftFighter');
-            console.log(`Posición R ${xPosR} y l  ${xPosL}`)
-            break;
-
-        case 68: // LIGHT LEFT ATTACK "D"
-            if (team2[1].life > 0 && team1[1].life > 0){
-                
+            case 68: // LIGHT LEFT ATTACK "D"
                 fighting('leftAttack');
-            }
-            break;
+                break;
 
-        case 80: // LIGHT RIGHT ATTACK "P"
-            if (team2[1].life > 0 && team1[1].life > 0){
-                
+            case 80: // LIGHT RIGHT ATTACK "P"
                 fighting('rightAttack'); 
-            }
-            break;
+                break;
 
-    //     case 79: // RIGHT DEFENSE"O"
-    //         defRight = 1;
-    //         console.log(defRight);
-    //         console.log(defLeft);
-    //         break;
+            case 83: // LEFT DEFENSE "S"
+                fighting('leftDefenseOn');
+                break;
 
-    //     case 83: // LEFT DEFENSE"O"
-    //         defLeft = 1;
-    //         console.log(defRight);
-    //         console.log(defLeft);
-    //         break;
+            case 79: // RIGHT DEFENSE "O"
+                fighting('rightDefenseOn');
+                break;
+        }
     }
 }
 
-// let defRight = 0;
-// let defLeft = 0;
-// const defense = (eventoPress) => {
-//     let eventoDef = window.event || eventoPress; //almacena info de la tecla
-//     switch(eventoDef.keyCode){
-//         case 79: // RIGHT DEFENSE"O"
-//             defRight = 1;
-//             console.log(defRight);
-//             console.log(defLeft);
-//             break;
-//         case 83: // LEFT DEFENSE"O"
-//             defLeft = 1;
-//             console.log(defRight);
-//             console.log(defLeft);
-//             break;
-//     }
-// }
+const stopAction = (elEvento2) => {
+    let eventoDef = window.event || elEvento2; //almacena info de la tecla
+    if (team2[1].life > 0 && team1[1].life > 0){
+        switch(eventoDef.keyCode){
+        //     case 39: //DERECHA LEFT FLECHA DERECHA
+        //     if (xPosR<59){
+        //         xPosR+=velX;
+        //     }
+        //     console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+        //     movementX(xPosR, 'rightFighter');
+        //     console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+        //     break;
+
+        // case 37: //IZQUIERDA LEFT FLECHA IZQUIERDA
+        //     if (xPosR>0){
+        //         xPosR-=velX;
+        //     }
+        //     console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+        //     movementX(xPosR, 'rightFighter');
+        //     console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+        //     break;
+
+        // case 66: // DERECHA RIGHT "B"
+        //     if (xPosL<59){
+        //         xPosL+=velX;
+        //     }
+        //     console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+        //     movementX(xPosL, 'leftFighter');
+        //     console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+        //     break;
+
+        // case 86: // IZQUIERDA RIGHT "V"
+        //     if (xPosL>0){
+        //         xPosL-=velX;
+        //     }
+        //     console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+        //     movementX(xPosL, 'leftFighter');
+        //     console.log(`Posición R ${xPosR} y l  ${xPosL}`)
+        //     break;
+
+        case 68: // LIGHT LEFT ATTACK "D"
+            fighting('leftAttackFrameOff');
+            break;
+
+        case 80: // LIGHT RIGHT ATTACK "P"
+            fighting('rightAttackFrameOff'); 
+            break;
+        
+        case 83: // LEFT DEFENSE "S"
+            fighting('leftDefenseOff');
+            break;
+
+        case 79: // RIGHT DEFENSE "O"
+            fighting('rightDefenseOff');
+            break;
+        }
+    }
+}
 
 
-// const validationPush = () => {
-//     let validateScreen = document.getElementById('fightScreen');
-//     return validateScreen;
-// }
 
 function systemOn(){
     document.onkeydown = movementAndAttack; //capta la pulsación de la tecla y llama a la función pusher
+    document.onkeyup = stopAction;
 };
 systemOn();
 
@@ -209,23 +238,39 @@ const placeFighter = (index) => {
     leftF = team1[index];
     rightF = team2[index];
 
-    document.getElementById('leftFighter').innerHTML = `<img id="${leftF.id}" src="assets/fighters/${leftF.colour}1.png" class="characterPic" alt="Player${leftF.id}">`;
-    document.getElementById('rightFighter').innerHTML = `<img id="${rightF.id}" src="assets/fighters/${rightF.colour}1.png" class="characterPic" alt="Player${rightF.id}">`; 
+    document.getElementById('leftFighter').innerHTML = `<img id="${leftF.id}" src="assets/fighters/${leftF.colour}GuardGif.gif" class="characterPic" alt="Player${leftF.id}">`;
+    document.getElementById('rightFighter').innerHTML = `<img id="${rightF.id}" src="assets/fighters/${rightF.colour}GuardGif.gif" class="characterPic" alt="Player${rightF.id}">`; 
 }
 const changeDeadFighterLeft = (index, positionF) => {
     deadF = team1[index];
     document.getElementById(positionF).innerHTML = "";
-    document.getElementById(positionF).innerHTML = `<img id="${deadF.id}" src="assets/fighters/${deadF.colour}1.png" class="characterPic" alt="Player${deadF.id}">`;
+    document.getElementById(positionF).innerHTML = `<img id="${deadF.id}" src="assets/fighters/${deadF.colour}GuardGif.gif" class="characterPic" alt="Player${deadF.id}">`;
 }
 const changeDeadFighterRight = (index, positionF) => {
     dead = team2[index];
     document.getElementById(positionF).innerHTML = "";
-    document.getElementById(positionF).innerHTML = `<img id="${dead.id}" src="assets/fighters/${dead.colour}1.png" class="characterPic" alt="Player${dead.id}">`;
+    document.getElementById(positionF).innerHTML = `<img id="${dead.id}" src="assets/fighters/${dead.colour}GuardGif.gif" class="characterPic" alt="Player${dead.id}">`;
 }
+const attackFrameOn = (fighter, position) => {
+    document.getElementById(position).innerHTML = `<img id="${fighter.id}" src="assets/fighters/${fighter.colour}Attack.png" class="characterPic" alt="Player${fighter.id}">`;
+}
+const attackFrameOff = (fighter, position) => {
+    document.getElementById(position).innerHTML = `<img id="${fighter.id}" src="assets/fighters/${fighter.colour}GuardGif.gif" class="characterPic" alt="Player${fighter.id}">`;
+}
+const defenseFrameOn = (fighter, position) => {
+    document.getElementById(position).innerHTML = `<img id="${fighter.id}" src="assets/fighters/${fighter.colour}DefGif.gif" class="characterPic" alt="Player${fighter.id}">`;
+}
+const defenseFrameOff = (fighter, position) => {
+    document.getElementById(position).innerHTML = `<img id="${fighter.id}" src="assets/fighters/${fighter.colour}GuardGif.gif" class="characterPic" alt="Player${fighter.id}">`;
+}
+
+
 
 /* FIGHT ACTION */
 let countLeft=0;
 let countRight=0;
+let defenseLeft=0;
+let defenseRight=0;
 const fighting = (action) => {
     p1 = team1[countLeft];
     p2 = team2[countRight];
@@ -238,24 +283,61 @@ const fighting = (action) => {
     let posXRight;
     posXRight = fighterRight.style.left;
 
+    if (action == 'leftAttackFrameOff'){
+        if (defenseLeft==0){
+            attackFrameOff(p1, 'leftFighter');
+        }
+    }
+
+    if (action == 'rightAttackFrameOff'){
+        if (defenseRight==0){
+            attackFrameOff(p2, 'rightFighter');
+        }
+    }
+
+    if (action == 'leftDefenseOff'){
+        defenseLeft=0;
+        defenseFrameOff(p1, 'leftFighter');
+
+    }
+    if (action == 'rightDefenseOff'){
+        defenseRight=0;
+        defenseFrameOff(p2, 'rightFighter');
+        
+    }
+    if (action == 'leftDefenseOn'){
+        defenseLeft=1;
+        console.log('Defense Left '+defenseLeft)
+        defenseFrameOn(p1, 'leftFighter');
+        
+    }
+    if (action == 'rightDefenseOn'){
+        defenseRight=1;
+        console.log('Defense Right ' +defenseRight);
+        defenseFrameOn(p2, 'rightFighter');
+        
+    }
+
     if (action == 'leftAttack'){
-        console.log(posXLeft)
-        if (posXLeft=='66%'){
-            if (posXRight=='0%'){
+        if (defenseLeft==0){
+            attackFrameOn(p1, 'leftFighter');
+            if (posXLeft=='66%' && posXRight=='0%' && defenseRight==0){
                 p1.hit(p2);
                 bar(p2, 'lifeBarRight');
-            }
+            }    
         }
     }
     if (action == 'rightAttack'){
-        console.log(posXRight)
-        if (posXRight=='0%'){
-            if (posXLeft=='66%'){
+        if (defenseRight==0){
+            attackFrameOn(p2, 'rightFighter');
+            if (posXRight=='0%' && posXLeft=='66%' && defenseLeft==0){
                 p2.hit(p1);
                 bar(p1, 'lifeBarLeft');
             }
         }
     }
+    
+
 
     console.log(`${p1.lordName} tiene: ${p1.life} de vida`);
     console.log(`${p2.lordName} tiene ${p2.life} de vida`);
